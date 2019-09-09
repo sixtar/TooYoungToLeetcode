f45e4d988d3664602114cfcec44c564d660d5c14
DingDing <sixtar.org@gmail.com>
2019-09-10 01:23:04 +0800

    1. add "add two number" two kinds of solutions

---
+++ src/_2_Add_two_Numbers/AddTwoNumbers1.java
@@ -0,0 +1,112 @@
+package _2_Add_two_Numbers;
+
+/**
+ * Created by dzj on 2019.9.10.
+ */
+public class AddTwoNumbers1 {
+
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        //入参校验
+        if (null == l1) {
+            return l2;
+        }
+        if (null == l2) {
+            return l1;
+        }
+        //结果的头部节点
+        ListNode dummy = new ListNode(0);
+        //节点当前位置
+        ListNode currNode = dummy;
+        //默认进位置为0
+        int carry = 0;
+        //l1和l2都还没到结尾的情况下
+        while(l1 != null && l2 != null){
+            //先计算l1,l2,carry累加的结果
+            int addRslt = l1.val+l2.val+carry;
+            //计算结果链表当前位的值
+            int cVal = addRslt%10;
+            //计算结果链表当前位置的进位值
+            carry = addRslt/10;
+            ListNode aRsltNode = new ListNode(cVal);
+            currNode.next = aRsltNode;
+            currNode = aRsltNode;
+            l2 = l2.next;
+            l1 = l1.next;
+        }
+        // l2提前完成的情况下
+        while(l1 !=null){
+            //l1和进位计算累加值
+            int addRslt = l1.val+carry;
+            //计算结果链表当前节点的值
+            int cVal = addRslt%10;
+            //计算进位
+            carry = addRslt/10;
+            ListNode aRsltNode = new ListNode(cVal);
+            currNode.next = aRsltNode;
+            currNode = aRsltNode;
+            l1 = l1.next;
+        }
+
+        //l1提前完成的情况下
+        while(l2 !=null){
+            //l2和进位计算累加值
+            int addRslt = l2.val+carry;
+            //计算结果链表当前节点的值
+            int cVal = addRslt%10;
+            //计算进位
+            carry = addRslt/10;
+            ListNode aRsltNode = new ListNode(cVal);
+            currNode.next = aRsltNode;
+            currNode = aRsltNode;
+            l2 = l2.next;
+        }
+        //注意最后还有个进位要处理
+        if(carry != 0){
+            currNode.next = new ListNode(carry);
+        }
+        //跳过dummy节点返回链表
+        return dummy.next;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+        }
+    }
+
+    private static void printListNode(ListNode head){
+        StringBuilder sb = new StringBuilder("Node: ");
+        if(head != null){
+            sb.append(head.val);
+            while(head.next != null){
+                sb.append("->"+head.next.val);
+                head = head.next;
+            }
+        }else{
+            sb.append("EMPTY!");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        // l1 2->4->3
+        ListNode l1Head = new ListNode(2);
+        l1Head.next = new ListNode(4);
+        l1Head.next.next = new ListNode(3);
+        //l2 5->6->4
+        ListNode l2Head = new ListNode(5);
+        l2Head.next = new ListNode(6);
+        l2Head.next.next = new ListNode(4);
+        ListNode listNode = addTwoNumbers(l1Head, l2Head);
+
+        //输出结果 预期应该为Node: 7->0->8
+        printListNode(listNode);
+
+    }
+}
---
+++ src/_2_Add_two_Numbers/AddTwoNumbers2.java
@@ -0,0 +1,79 @@
+package _2_Add_two_Numbers;
+
+/**
+ * Created by dzj on 2019.9.10.
+ */
+public class AddTwoNumbers2 {
+
+
+    /**
+     * 第二种方法更简洁
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        //三个条件任意条件满足都会继续计算
+        while (l1 != null || l2 != null || carry != 0) {
+            ListNode cur = new ListNode(0);
+            //三元运算符分别判断
+            int sum = ((l2 == null) ? 0 : l2.val)
+                    + ((l1 == null) ? 0 : l1.val)
+                    + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+
+            l1 = (l2 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+
+        return head.next;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    private static void printListNode(ListNode head) {
+        StringBuilder sb = new StringBuilder("Node: ");
+        if (head != null) {
+            sb.append(head.val);
+            while (head.next != null) {
+                sb.append("->" + head.next.val);
+                head = head.next;
+            }
+        } else {
+            sb.append("EMPTY!");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        // l1 2->4->3
+        ListNode l1Head = new ListNode(2);
+        l1Head.next = new ListNode(4);
+        l1Head.next.next = new ListNode(3);
+        //l2 5->6->4
+        ListNode l2Head = new ListNode(5);
+        l2Head.next = new ListNode(6);
+        l2Head.next.next = new ListNode(4);
+        ListNode listNode = addTwoNumbers(l1Head, l2Head);
+
+        //输出结果 预期应该为Node: 7->0->8
+        printListNode(listNode);
+
+    }
+}
