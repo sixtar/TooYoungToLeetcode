132e36ce56fc4bbc8bb44d85b4c5f53ed9f469e6
DingDing <sixtar.org@gmail.com>
2020-05-07 22:59:05 +0800

    add "Integer to Roman" solution1

--- src/_012_Integer_to_Roman/Solution.java
+++
@@ -1,8 +0,0 @@
-package _012_Integer_to_Roman;
-
-/**
- * 整形数转罗马 TODO
- * Created by dzj on 2020.5.7.
- */
-public class Solution {
-}
---
+++ src/_012_Integer_to_Roman/Solution1.java
@@ -0,0 +1,93 @@
+package _012_Integer_to_Roman;
+
+/**
+ * 整形数转罗马
+ * Created by dzj on 2020.5.7.
+ */
+public class Solution1 {
+
+    /**
+     * 只会暴力解决...看题解应该用贪心
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+
+        StringBuilder sb = new StringBuilder();
+        while(num > 0){
+            if(num >= 1000){
+                sb.append("M");
+                num -= 1000;
+                continue;
+            }
+            if(num >= 900){
+                sb.append("CM");
+                num -= 900;
+                continue;
+            }
+            if(num >= 500){
+                sb.append("D");
+                num -= 500;
+                continue;
+            }
+            if(num >= 400){
+                sb.append("CD");
+                num -= 400;
+                continue;
+            }
+            if(num >= 100){
+                sb.append("C");
+                num -= 100;
+                continue;
+            }
+            if(num >= 90){
+                sb.append("XC");
+                num -= 90;
+                continue;
+            }
+            if(num >= 50){
+                sb.append("L");
+                num -= 50;
+                continue;
+            }
+            if(num >= 40){
+                sb.append("XL");
+                num -= 40;
+                continue;
+            }
+            if(num >= 10){
+                sb.append("X");
+                num -= 10;
+                continue;
+            }
+            if(num >= 9){
+                sb.append("IX");
+                num -= 9;
+                continue;
+            }
+            if(num >= 5){
+                sb.append("V");
+                num -= 5;
+                continue;
+            }
+            if(num >= 4){
+                sb.append("IV");
+                num -= 4;
+                continue;
+            }
+            while(num > 0){
+                sb.append("I");
+                num--;
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        //int i = 1994;
+        int i = 20;
+        System.out.println("new Solution().intToRoman(i) = " + new Solution1().intToRoman(i));
+    }
+}
